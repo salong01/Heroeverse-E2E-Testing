2ba8f0382f7c4191706b776177320fb141463dbb
salong01 <salong01@estudiantes.unileon.es>
2021-06-21 12:59:30 +0200

    Test T007 and T008 implemented and added all test to test suite

---
+++ src/main/java/e2e/testing/test/T007WrongRegister.java
@@ -0,0 +1,41 @@
+package e2e.testing.test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+
+import e2e.testing.core.CoreTest;
+import e2e.testing.pages.RegisterPage;
+
+public class T007WrongRegister extends CoreTest{
+
+	private String user;
+	private String email;
+	private String pass;
+	
+	@Override
+	public void startTest() {
+		user = "saul";
+		email = "saul.alonso.glez@gmail.com";
+		pass = "saul";
+	}
+
+	@Override
+	public void runTest() {
+
+		RegisterPage register = new RegisterPage(getDriver());
+		
+		getLogger().add("Register user " + user);
+		register.register(user, email, pass, pass);
+		
+		getLogger().add("Check if user " + user + " can see the menu");
+		Assert.assertFalse(register.isUserLogged());
+		
+		getLogger().add("Chech that url is the register one");
+		assertTrue(getDriver().getCurrentUrl().equals("http://localhost:4000/register"));
+	}
+	
+	@Override
+	public void endTest() {
+	}
+}
---
+++ src/main/java/e2e/testing/test/T008WrongLogin.java
@@ -0,0 +1,38 @@
+package e2e.testing.test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import e2e.testing.core.CoreTest;
+import e2e.testing.pages.LoginPage;
+
+public class T008WrongLogin extends CoreTest{
+
+	private String user;
+	private String pass;
+	
+	@Override
+	public void startTest() {
+		user = "saul2";
+		pass = "saul";
+	}
+
+	@Override
+	public void runTest() {
+		
+		LoginPage login = new LoginPage(getDriver());
+		
+		getLogger().add("Go to login page and login with user " + user);
+		login.login(user, pass);
+		
+		getLogger().add("Check if user " + user + " can see the menu");
+		assertFalse(login.isUserLogged(), "user not logged");
+		
+		getLogger().add("Chech that url is the login one");
+		assertTrue(getDriver().getCurrentUrl().equals("http://localhost:4000/login"));
+	}
+
+	@Override
+	public void endTest() {
+	}
+}
